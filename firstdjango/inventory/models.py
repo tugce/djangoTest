dccfed6c07abf260ae22b2eaa640ed6785d29c17
Tugce Sirin <ztugcesirin@gmail.com>
2015-10-19 21:30:09 +0300

    typo düzeltildi

--- firstdjango/inventory/models.py
+++ firstdjango/inventory/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Item(models.Model):
-    title= models.charField(max-lenght=200)
+    title= models.charField(max_lenght=200)
     description = models.TextField()
     amount = models.ImtegerField()
