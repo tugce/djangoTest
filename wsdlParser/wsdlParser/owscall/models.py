1ef0676dddad48ac3e66a5d20d523a1158d6a887
Tugce Sirin <ztugcesirin@gmail.com>
2015-10-24 01:18:06 +0300

    modeller eklendi

--- wsdlParser/wsdlParser/owscall/models.py
+++ wsdlParser/wsdlParser/owscall/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Wsdl(models.Model):
+    name = models.CharField(max_length=200)
+    endpoint = models.TextField()
+
+class Method(models.Model):
+    wsdlId = models.ForeignKey('Wsdl.Id')
+    name = models.CharField(max_length=200)
+    paremeters = models.TextField()
